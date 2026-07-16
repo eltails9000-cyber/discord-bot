26c0d89998aa4428883ffb1c80c34d95749b669c
eltails9000-cyber <eltails9000@gmail.com>
2026-07-16 15:31:05 -0500

    Implement maintenance mode for bot commands
    Added maintenance mode handling for commands.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import discord
 from config import TOKEN
 
+from utils import maintenance_state
+
+
 intents = discord.Intents.all()
+
 
 bot = discord.Bot(
     intents=intents
 )
+
 
 @bot.event
 async def on_ready():
@@ -23,11 +28,42 @@ async def on_ready():
     await bot.sync_commands()
 
 
+
+@bot.event
+async def on_application_command(ctx):
+
+    command = ctx.command.name
+
+
+    # Comandos que siguen funcionando en mantenimiento
+    allowed_commands = [
+        "maintenance",
+        "botstatus",
+        "restart",
+        "shutdown"
+    ]
+
+
+    if maintenance_state.maintenance_mode:
+
+        if command not in allowed_commands:
+
+            await ctx.respond(
+                "🔴 **Bot en mantenimiento.**\n"
+                "Intenta más tarde.",
+                ephemeral=True
+            )
+
+            raise discord.CheckFailure
+
+
+
 # Cargar comandos
 bot.load_extension("cogs.roblox")
 bot.load_extension("cogs.owner")
 bot.load_extension("cogs.maintenance")
 bot.load_extension("cogs.moderation")
+
 
 # Iniciar bot
 bot.run(TOKEN)
