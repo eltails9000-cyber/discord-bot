318a1db7bda5a31296797e01c9819d4a1cb83373
eltails9000-cyber <eltails9000@gmail.com>
2026-07-16 17:48:41 -0500

    Refactor environment variable assignments in config.py

--- config.py
+++ config.py
@@ -1,15 +1,21 @@
 import os
 
 
-TOKEN = os.getenv("DISCORD_TOKEN")
-
-ROBLOX_KEY = os.getenv(
-    "ROBLOX_API_KEY"
+# Discord Bot Token
+TOKEN = os.getenv(
+    "DISCORD_TOKEN"
 )
 
+
+# Glory or Death API
 API_URL = os.getenv(
     "API_URL"
 )
 
+API_KEY = os.getenv(
+    "ROBLOX_API_KEY"
+)
 
+
+# Owner
 OWNER_ID = 1042929958286266540
