c98185b986f4c012ad9450b9dddcedd1247a945b
eltails9000-cyber <eltails9000@gmail.com>
2026-07-16 15:10:18 -0500

    Refactor bot initialization and command loading

--- main.py
+++ main.py
@@ -1,40 +1,31 @@
 import discord
-import os
-
-
 from config import TOKEN
 
-
 intents = discord.Intents.all()
-
 
 bot = discord.Bot(
     intents=intents
 )
 
-
-
 @bot.event
 async def on_ready():
 
-    print(
-        f"✅ Online como {bot.user}"
+    await bot.change_presence(
+        status=discord.Status.dnd,
+        activity=discord.Activity(
+            type=discord.ActivityType.watching,
+            name="Glory or Death"
+        )
     )
+
+    print(f"✅ Online como {bot.user}")
 
     await bot.sync_commands()
 
 
+# Cargar comandos
+bot.load_extension("cogs.roblox")
+bot.load_extension("cogs.moderation")
 
-# cargar comandos
-
-bot.load_extension(
-    "cogs.roblox"
-)
-
- bot.load_extension(
-     "cogs.moderation"
- )
-
-
-
+# Iniciar bot
 bot.run(TOKEN)
